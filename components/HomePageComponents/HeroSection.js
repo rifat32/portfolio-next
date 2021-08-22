899ec985032787d567b1bfb51ca9b024cd8d2f6d
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-08-22 12:26:08 +0600

    final

--- components/HomePageComponents/HeroSection.js
+++ components/HomePageComponents/HeroSection.js
@@ -1,9 +1,11 @@
 import Typed from 'react-typed';
+import heroImg from '../../assets/img/hero-bg.jpg';
+import Image from 'next/image'
 
 const HeroSection = () => {
     return (
         <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
-  <div className="hero-container" data-aos="fade-in">
+  {/* <div className="hero-container" data-aos="fade-in">
     <h1>RIFAT AL ASHWAD</h1>
     <p>I'm {" "}
      <Typed
@@ -14,6 +16,20 @@ const HeroSection = () => {
                     loop
                 />
    </p>
+  </div> */}
+  <Image src={heroImg} className="img-fluid" alt="profile-pic" quality={50} placeholder="blur"  layout="fill" />
+  <div className="hero-container centered">
+  <h1>RIFAT AL ASHWAD</h1>
+    <p>I'm {" "}
+     <Typed
+                    strings={['Back-End  Web Developer','Front-End Web Developer']}
+                    typeSpeed={40}
+                    backSpeed={50}
+                
+                    loop
+                />
+   </p>
+  
   </div>
 </section>
     )
