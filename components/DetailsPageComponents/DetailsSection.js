1bcdecccab8130dc74acf9537978ffe5fad3f660
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-08-22 23:22:44 +0600

    final

--- components/DetailsPageComponents/DetailsSection.js
+++ components/DetailsPageComponents/DetailsSection.js
@@ -1,19 +1,65 @@
 
-
+import Image from 'next/image'
 
 
 
 
 const DetailsSection = ({portfolio}) => {
  
-  if(portfolio){
     return (
    
-
+      <section id="portfolio-details" className="portfolio-details">
+      <div className="container">
+        <div className="row gy-4">
+          <div className="col-lg-8">
+         
+              <div 
+           className="portfolio-details-img">
+          
+              
+               <Image src={portfolio.image} alt="profile-pic" quality={1} placeholder="blur"  layout="responsive" priority />
+              
+         
+              
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="portfolio-info">
+              <h3>Project information</h3>
+              <ul>
+                <li><strong>Category</strong>: {portfolio.category}</li>
+                <li><strong>Project URL</strong>: <a href={portfolio.project_url} target="_blank" className="btn btn-info">Visite</a></li>
+                {
+                  portfolio.github_link &&   <li><strong>Github Link</strong>: <a href={portfolio.github_link} target="_blank" className="btn btn-info">Visite</a></li>
+                }
+                {
+                  portfolio.github_frontend &&     <li><strong>Github Front-End</strong>: <a href={portfolio.github_frontend} target="_blank" className="btn btn-info">Visite</a></li>
+                }
+                {
+                  portfolio.github_backend &&    <li><strong>Github Back-End</strong>: <a href={portfolio.github_backend} target="_blank" className="btn btn-info">Visite</a></li>
+                }
+              
+              
+            
+     
+              </ul>
+            </div>
+           
+          </div>
+          <div className="col-10 ">
+            
+            <div className="portfolio-description">
+              <h2>Details</h2>
+              <p>
+              {portfolio.description}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+     </section>
    )
-  } else {
-    return (<div></div>)
-  }
+   
 
  
 }
--- pages/portfolio-details/[slug].js
+++ pages/portfolio-details/[slug].js
@@ -1,68 +1,25 @@
 import BreadcrumbsSection from '../../components/DetailsPageComponents/BreadcrumbsSection'
 import {useRouter} from 'next/router';
 import portfolios from "../../data/portfolio/PortfolioData"
-import Image from 'next/image'
+import DetailsSection from '../../components/DetailsPageComponents/DetailsSection';
 
 
-const DetaisPage = () => {
-const router  = useRouter();
-  const portfolio = portfolios.find(el => (el.slug === router.query.slug))
+
+const DetaisPage = ({portfolio}) => {
 
     return (
         <main id="main">
            <BreadcrumbsSection/>
-           <section id="portfolio-details" className="portfolio-details">
- <div className="container">
-   <div className="row gy-4">
-     <div className="col-lg-8">
-    
-         <div 
-      className="portfolio-details-img">
-     
-         
-          <Image src={portfolio.image} alt="profile-pic" quality={1} placeholder="blur"  layout="responsive" priority />
-         
-    
-         
-       </div>
-     </div>
-     <div className="col-lg-4">
-       <div className="portfolio-info">
-         <h3>Project information</h3>
-         <ul>
-           <li><strong>Category</strong>: {portfolio.category}</li>
-           <li><strong>Project URL</strong>: <a href={portfolio.project_url} target="_blank" className="btn btn-info">Visite</a></li>
-           {
-             portfolio.github_link &&   <li><strong>Github Link</strong>: <a href={portfolio.github_link} target="_blank" className="btn btn-info">Visite</a></li>
-           }
-           {
-             portfolio.github_frontend &&     <li><strong>Github Front-End</strong>: <a href={portfolio.github_frontend} target="_blank" className="btn btn-info">Visite</a></li>
-           }
-           {
-             portfolio.github_backend &&    <li><strong>Github Back-End</strong>: <a href={portfolio.github_backend} target="_blank" className="btn btn-info">Visite</a></li>
-           }
-         
-         
-       
-
-         </ul>
-       </div>
-      
-     </div>
-     <div className="col-10 ">
-       
-       <div className="portfolio-description">
-         <h2>Details</h2>
-         <p>
-         {portfolio.description}
-         </p>
-       </div>
-     </div>
-   </div>
- </div>
-</section>
+           <DetailsSection portfolio={portfolio}/>
         </main>
     )
 }
-
+export async function getServerSideProps({query}) {
+  const portfolio = portfolios.find(el => (el.slug === query.slug))
+  return {
+     props:{
+      portfolio
+     }
+   }
+ }
 export default DetaisPage
